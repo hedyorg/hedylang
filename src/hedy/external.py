ea29a202309b65957d50bf3a7655cf3b867e1071
Rico Hermans <rix0rrr@gmail.com>
2026-03-15 20:39:17 +0100

    fix: gettext cannot be overridden

--- src/hedy/external.py
+++ src/hedy/external.py
@@ -1,11 +1,13 @@
 """Functions that can be configured by the user of this library, such as the gettext function for translation."""
 
+THE_GETTEXT = lambda x: x
+
 def initialize_gettext(gettext_fn):
     """Initialize the gettext function used for translation in this module."""
-    global gettext
-    gettext = gettext_fn
+    global THE_GETTEXT
+    THE_GETTEXT = gettext_fn
 
 
 def gettext(s):
     """Default gettext function that just returns the input string."""
-    return s
+    return THE_GETTEXT(s)
